406754d0b1de9a8dba32ef02415e96e36f1392bb
Jakub <3611452+Jack8472@users.noreply.github.com>
2022-04-17 22:06:04 +0200

    Update in-chroot.sh

--- in-chroot.sh
+++ in-chroot.sh
@@ -7,7 +7,7 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=pl" >> /etc/vconsole.conf
 
-pacman -S grub efibootmgr networkmanager network-manager-applet base-devel linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack reflector plocate ufw xorg-server ttf-iosevka-nerd qtile picom git fish sudo btrfs-progs vim xfce4 lightmd lightdm-gtk-greeter alacritty emacs ttc-iosevka-aile xorg-server firefox
+pacman -S grub efibootmgr networkmanager network-manager-applet base-devel linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack reflector plocate ufw xorg-server ttf-iosevka-nerd qtile picom git fish sudo btrfs-progs vim xfce4 lightdm lightdm-gtk-greeter alacritty emacs ttc-iosevka-aile xorg-server firefox
 
 # xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils 
 
@@ -24,6 +24,7 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable lightdm.service
 systemctl enable ufw.service
+systemctl enable plocate-updatedb.timer
 stemctl enable btrfs-scrub@-.timer 
 systemctl enable btrfs-scrub@home.timer 
 
