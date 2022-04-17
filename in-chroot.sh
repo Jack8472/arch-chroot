4c59a9c5c5a2a764f6e4f7a840e48e1d3ecf7630
Jakub <3611452+Jack8472@users.noreply.github.com>
2022-04-17 22:30:52 +0200

    Update in-chroot.sh

--- in-chroot.sh
+++ in-chroot.sh
@@ -9,15 +9,14 @@ echo "KEYMAP=pl" >> /etc/vconsole.conf
 
 pacman -S grub efibootmgr networkmanager network-manager-applet base-devel linux-headers pipewire pipewire-alsa pipewire-pulse pipewire-jack reflector plocate ufw xorg-server ttf-iosevka-nerd qtile picom git fish sudo btrfs-progs vim xfce4 lightdm lightdm-gtk-greeter alacritty emacs ttc-iosevka-aile xorg-server firefox
 
-# xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils 
-
 # optional
+
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 # pacman -S --noconfirm intel-ucode
 # pacman -S --noconfirm amd-ucode
 
-mkinitcpio -P
+passwd
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -31,4 +30,4 @@ systemctl enable plocate-updatedb.timer
 stemctl enable btrfs-scrub@-.timer 
 systemctl enable btrfs-scrub@home.timer 
 
-printf "Next: set root password; set hostname; add user and make sudoer."
+printf "Next: set hostname; add user+pass and make sudoer."
