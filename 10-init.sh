0741002bf944c5249648196ff8acdb22c08638f6
Jakub <3611452+Jack8472@users.noreply.github.com>
2022-04-23 09:22:46 +0200

    Update 10-init.sh

--- 10-init.sh
+++ 10-init.sh
@@ -15,6 +15,7 @@ pacstrap /mnt base linux linux-firmware vim reflector grub btrfs-progs
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp $SCRIPT_DIR/20-chroot.sh /mnt/
 chmod +x /mnt/20-chroot.sh
 
