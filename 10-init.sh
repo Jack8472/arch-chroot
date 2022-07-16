26ab09ffebd230f4334395d0be3f64989f0cce47
Jakub <3611452+Jack8472@users.noreply.github.com>
2022-07-16 13:14:42 +0200

    Update 10-init.sh

--- 10-init.sh
+++ 10-init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-figlet "Checking mounts..."
+echo  "=> $(tput setaf 2 bold) Checking mounts..."
 if ! grep -qs '/mnt' /proc/mounts; then
     echo "Drives not mounted, will not continue."
     exit 2
@@ -8,21 +8,24 @@ fi
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )"
 
-figlet "Adjusting pacman configuration and configuring mirrors."
+echo  "=> $(tput setaf 2 bold) Adjusting pacman configuration and configuring mirrors..."
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 sed -i 's/^#Color/Color/' /etc/pacman.conf
 reflector --latest 20 --protocol https --sort rate --number 5 --save /etc/pacman.d/mirrorlist
-figlet "Updating pacman db..."
+
+echo  "=> $(tput setaf 2 bold) Updating pacman db..."
 pacman -Syy
-figlet "Pacstrap..."
+
+echo  "=> $(tput setaf 2 bold) Pacstrap..."
 pacstrap /mnt base linux linux-firmware vim reflector grub btrfs-progs
-figlet "genfstab..."
+
+echo  "=> $(tput setaf 2 bold) genfstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
-figlet "copy mirrorlist..."
+
+echo  "=> $(tput setaf 2 bold) copy mirrorlist..."
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 figlet "cp & +x 20-chroot.sh..."
 cp $SCRIPT_DIR/20-chroot.sh /mnt/
 chmod +x /mnt/20-chroot.sh
 
-figlet "DONE"
-echo "DO: arch-chroot /mnt"
+echo  "=> $(tput setaf 1 bold) DO: arch-chroot /mnt"
