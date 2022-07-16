04c4d6c4929bd3ca05c888b6b44a7b4676fe9716
Jakub <3611452+Jack8472@users.noreply.github.com>
2022-07-16 12:53:14 +0200

    Update 10-init.sh

--- 10-init.sh
+++ 10-init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Checking mounts..."
+figlet "Checking mounts..."
 if ! grep -qs '/mnt' /proc/mounts; then
     echo "Drives not mounted, will not continue."
     exit 2
@@ -8,20 +8,21 @@ fi
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )"
 
-echo "Adjusting pacman configuration and configuring mirrors."
+figlet "Adjusting pacman configuration and configuring mirrors."
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 sed -i 's/^#Color/Color/' /etc/pacman.conf
 reflector --latest 20 --protocol https --sort rate --number 5 --save /etc/pacman.d/mirrorlist
-echo "Updating pacman db..."
+figlet "Updating pacman db..."
 pacman -Syy
-echo "Pacstrap..."
+figlet "Pacstrap..."
 pacstrap /mnt base linux linux-firmware vim reflector grub btrfs-progs
-echo "genfstab..."
+figlet "genfstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
-echo "copy mirrorlist..."
+figlet "copy mirrorlist..."
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-echo "cp & +x 20-chroot.sh..."
+figlet "cp & +x 20-chroot.sh..."
 cp $SCRIPT_DIR/20-chroot.sh /mnt/
 chmod +x /mnt/20-chroot.sh
 
+figlet "DONE"
 echo "DO: arch-chroot /mnt"
